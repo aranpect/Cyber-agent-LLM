7faddd6afea938d38f5717c63115b26f9871bf78
aranpect <120459638+aranpect@users.noreply.github.com>
2023-11-08 15:55:06 +0900

    Update main.py

--- main.py
+++ main.py
@@ -8,27 +8,24 @@
 import transformers
 from transformers import AutoModelForCausalLM, AutoTokenizer, TextStreamer
 
+import transformers
+from transformers import AutoModelForCausalLM, AutoTokenizer, TextStreamer
+
+assert transformers.__version__ >= "4.34.1"
+
 model = AutoModelForCausalLM.from_pretrained("cyberagent/calm2-7b-chat", device_map="auto", torch_dtype="auto", offload_folder="/path/to/offload_folder")
 tokenizer = AutoTokenizer.from_pretrained("cyberagent/calm2-7b-chat")
 streamer = TextStreamer(tokenizer, skip_prompt=True, skip_special_tokens=True)
 
-def llm():
-    question = input("質問を入力してください:")
-    token_ids = tokenizer.encode(prompt, return_tensors="pt")
-    output_ids = model.generate(
-        input_ids=token_ids.to(model.device),
-        max_new_tokens=300,
-        do_sample=True,
-        temperature=0.8,
-        streamer=streamer,
-    )
+prompt = """USER: AIによって私達の暮らしはどのように変わりますか？
+ASSISTANT: """
 
-    response = tokenizer.decode(output_ids[0], skip_special_tokens=True)
-    return response
-
-while True:
-  judge = input("質問を続ける場合は0を入力してください。終了する場合は1を入力してください")
-  if(judge=="0"):
-    llm()
-  else:
-    break
+token_ids = tokenizer.encode(prompt, return_tensors="pt")
+output_ids = model.generate(
+    input_ids=token_ids.to(model.device),
+    max_new_tokens=300,
+    do_sample=True,
+    temperature=0.8,
+    streamer=streamer,
+)
+reak
